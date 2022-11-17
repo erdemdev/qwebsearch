092c2ab9fc65103c463b3bcc1ef6e005f6f3cab2
Erdem Arslan <38112087+erdemdev@users.noreply.github.com>
2022-11-17 21:49:52 +0300

    defaultConfig logic

--- src/main.tsx
+++ src/main.tsx
@@ -4,21 +4,32 @@ import App from './App';
 import './style.css';
 import { writeTextFile, createDir, readTextFile } from '@tauri-apps/api/fs';
 import { appDir } from '@tauri-apps/api/path';
+import { getVersion } from '@tauri-apps/api/app';
+import defaultConfig from './config.json';
 
+// #region Configuration
 (async () => {
-  const path = await appDir();
+  const appVersion = await getVersion();
+  const appPath = await appDir();
 
   try {
-    const settings = JSON.parse(await readTextFile(path + '/settings.json'));
-    console.log(settings);
-  } catch (error) {
-    await createDir(path, { recursive: true });
-    await writeTextFile(
-      path + '/settings.json',
-      JSON.stringify({ test: true })
+    const savedConfig: typeof defaultConfig = JSON.parse(
+      await readTextFile(appPath + '/config.json')
     );
+
+    if (savedConfig.app.version !== appVersion)
+      throw 'App version is different.';
+  } catch (error) {
+    console.log(error + ' Creating a new config file.');
+
+    const newConfig = defaultConfig;
+    newConfig.app.version = appVersion;
+
+    await createDir(appPath, { recursive: true });
+    await writeTextFile(appPath + '/config.json', JSON.stringify(newConfig));
   }
 })();
+//#endregion
 
 // #region Disable WebView context menu on production.
 if (import.meta.env.PROD) {
