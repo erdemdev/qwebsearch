b3ff9070cc4bd35ce9289a159378244a4f95a242
Erdem Arslan <38112087+erdemdev@users.noreply.github.com>
2022-12-05 22:12:09 +0300

    doesnt need reducer

--- src/hooks/useConfig.tsx
+++ src/hooks/useConfig.tsx
@@ -4,8 +4,6 @@ import { getVersion } from '@tauri-apps/api/app';
 import defaultConfig from '@/default-config.json';
 import { useEffect, useState } from 'react';
 
-// TODO: Convert config into reducer.
-
 export default function useConfig(): [
   config: typeof config,
   setConfig: typeof setConfig,
